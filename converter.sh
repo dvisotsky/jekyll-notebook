4852252885a8ffef7253de42136239896ee1e6c8
David Visotsky <visotskydavid@gmail.com>
2024-04-24 18:06:20 -0400

    update notes

--- converter.sh
+++ converter.sh
@@ -1,7 +1,7 @@
 # convert markdown to jekyll note
 # Usage: ./converter.sh <markdown file>
 
-SOURCE_DIR=~/Documents/notebook/Studies
+SOURCE_DIR=~/Dropbox/notebook/Studies
 TARGET_DIR=./_notes/Public
 
 # Loop through each Markdown file in the source directory and its subdirectories
